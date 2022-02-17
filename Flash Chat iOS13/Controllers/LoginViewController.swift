8660150ea7e46105777c300729f8f586a3da55c0
kneskoromny <kneskoromny@gmail.com>
2022-02-17 15:40:54 +0300

    create, login, logout

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
+    
     @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text,
+           let password = passwordTextfield.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                guard let strongSelf = self else { return }
+                
+                if error != nil {
+                    print(error!)
+                } else {
+                    strongSelf.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
+
