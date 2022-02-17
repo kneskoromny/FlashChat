05ba4d3550e2821bb502c4dfbb914633f2b348ef
kneskoromny <kneskoromny@gmail.com>
2022-02-17 15:03:42 +0300

    firebase pod

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,26 +7,29 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for char in titleText {
-            Timer.scheduledTimer(
-                withTimeInterval: 0.1 * charIndex, repeats: false
-            ) { [weak self] timer in
-                
-                    self?.titleLabel.text?.append(char)
-                }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
+        
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for char in titleText {
+//            Timer.scheduledTimer(
+//                withTimeInterval: 0.1 * charIndex, repeats: false
+//            ) { [weak self] timer in
+//
+//                    self?.titleLabel.text?.append(char)
+//                }
+//            charIndex += 1
+//        }
     
     }
     // MARK: - UI Actions
