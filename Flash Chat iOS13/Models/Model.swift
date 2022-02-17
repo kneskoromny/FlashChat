3ec6725d2cb8ee5d5a8fe26bce0820d8f99e57f1
kneskoromny <kneskoromny@gmail.com>
2022-02-17 09:21:09 +0300

    additional branch test

---
+++ Flash Chat iOS13/Models/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  Flash Chat iOS13
+//
+//  Created by Кирилл Нескоромный on 17.02.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Model {
+    
+}
